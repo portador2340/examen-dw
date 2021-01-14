617f344e07aa5e563df04db3827b22b661b502eb
portador2340 <kartier_klever10@hotmail.com>
2021-01-14 10:27:50 -0500

    segundo commit

---
+++ pregunta1/js/suma.js
@@ -0,0 +1,274 @@
+function suma(){
+    var numero1=document.getElementById("numero1").value;
+    var numero2=document.getElementById("numero2").value;
+    var suma=parseInt(numero1)+parseInt(numero2);
+    document.getElementById("suma").innerHTML=mensaje;
+    var mensaje=suma; 
+
+}
+
+function resta(){
+
+    var numero1=document.getElementById("numero1").value;
+    var numero2=document.getElementById("numero2").value;
+    var resta=parseInt(numero1)-parseInt(numero2);
+    var mensaje=resta;
+    document.getElementById("resta").innerHTML=mensaje;
+}
+
+function divi(){
+
+    var numero1=document.getElementById("numero1").value;
+    var numero2=document.getElementById("numero2").value;
+    var div=parseInt(numero1)/parseInt(numero2);
+    var mensaje=div;
+    document.getElementById("divi").innerHTML=mensaje;
+
+}
+
+function resto(){
+    var numero1=document.getElementById("numero1").value;
+    var numero2=document.getElementById("numero2").value;
+    var resto=parseInt(numero1)%parseInt(numero2);
+    var mensaje=resto;
+    document.getElementById("resto").innerHTML=mensaje;
+
+
+
+}
+
+function nombre(){
+   var nombre=document.getElementById("nombre").value;
+    var mensaj=parseInt(nombre);
+    var men="bienvenido";
+    document.getElementById("mensaje").innerHTML=men+mensaj;
+
+
+
+}
+
+function coches(){
+
+    var coches=new Array();
+    var coche1=new Object();
+
+    coche1.marca="ford";
+    coche1.color="rojo";
+    coche1.placa="k 256";
+    coche1.motor="200 caballos";
+    coche1.precio="50 000";
+    coche1.pais="italia";
+    coche1.lugarCompra="online";
+
+    var coche2=new Object();
+
+    coche2.marca="ford";
+    coche2.color="rojo";
+    coche2.placa="k 256";
+    coche2.motor="200 caballos";
+    coche2.precio="50 000";
+    coche2.pais="italia";
+    coche2.lugarCompra="online";
+
+    var coche3=new Object();
+
+    coche3.marca="ford";
+    coche3.color="rojo";
+    coche3.placa="k 256";
+    coche3.motor="200 caballos";
+    coche3.precio="50 000";
+    coche3.pais="italia";
+    coche3.lugarCompra="online";
+
+    var coche4=new Object();
+
+    coche4.marca="ford";
+    coche4.color="rojo";
+    coche4.placa="k 256";
+    coche4.motor="200 caballos";
+    coche4.precio="50 000";
+    coche4.pais="italia";
+    coche4.lugarCompra="online";
+
+    var coche5=new Object();
+
+    coche5.marca="ford";
+    coche5.color="rojo";
+    coche5.placa="k 256";
+    coche5.motor="200 caballos";
+    coche5.precio="50 000";
+    coche5.pais="italia";
+    coche5.lugarCompra="online";
+
+    var coche6=new Object();
+
+    coche6.marca="ford";
+    coche6.color="rojo";
+    coche6.placa="k 256";
+    coche6.motor="200 caballos";
+    coche6.precio="50 000";
+    coche6.pais="italia";
+    coche6.lugarCompra="online";
+
+    var coche7=new Object();
+
+    coche7.marca="ford";
+    coche7.color="rojo";
+    coche7.placa="k 256";
+    coche7.motor="200 caballos";
+    coche7.precio="50 000";
+    coche7.pais="italia";
+    coche7.lugarCompra="online";
+
+    var coche8=new Object();
+
+    coche8.marca="ford";
+    coche8.color="rojo";
+    coche8.placa="k 256";
+    coche8.motor="200 caballos";
+    coche8.precio="50 000";
+    coche8.pais="italia";
+    coche8.lugarCompra="online";
+
+    var coche9=new Object();
+
+    coche9.marca="ford";
+    coche9.color="rojo";
+    coche9.placa="k 256";
+    coche9.motor="200 caballos";
+    coche9.precio="50 000";
+    coche9.pais="italia";
+    coche9.lugarCompra="online";
+
+    var coche10=new Object();
+
+    coche10.marca="ford";
+    coche10.color="rojo";
+    coche10.placa="k 256";
+    coche10.motor="200 caballos";
+    coche10.precio="50 000";
+    coche10.pais="italia";
+    coche10.lugarCompra="online";
+
+    var coche11=new Object();
+
+    coche11.marca="ford";
+    coche11.color="rojo";
+    coche11.placa="k 256";
+    coche11.motor="200 caballos";
+    coche11.precio="50 000";
+    coche11.pais="italia";
+    coche11.lugarCompra="online";
+
+    var coche12=new Object();
+
+    coche12.marca="ford";
+    coche12.color="rojo";
+    coche12.placa="k 256";
+    coche12.motor="200 caballos";
+    coche12.precio="50 000";
+    coche12.pais="italia";
+    coche12.lugarCompra="online";
+
+    var coche13=new Object();
+
+    coche13.marca="ford";
+    coche13.color="rojo";
+    coche13.placa="k 256";
+    coche13.motor="200 caballos";
+    coche13.precio="50 000";
+    coche13.pais="italia";
+    coche13.lugarCompra="online";
+
+    var coche14=new Object();
+
+    coche14.marca="ford";
+    coche14.color="rojo";
+    coche14.placa="k 256";
+    coche14.motor="200 caballos";
+    coche14.precio="50 000";
+    coche14.pais="italia";
+    coche14.lugarCompra="online";
+
+    var coche15=new Object();
+
+    coche15.marca="ford";
+    coche15.color="rojo";
+    coche15.placa="k 256";
+    coche15.motor="200 caballos";
+    coche15.precio="50 000";
+    coche15.pais="italia";
+    coche15.lugarCompra="online";
+
+    var coche16=new Object();
+
+    coche16.marca="ford";
+    coche16.color="rojo";
+    coche16.placa="k 256";
+    coche16.motor="200 caballos";
+    coche16.precio="50 000";
+    coche16.pais="italia";
+    coche16.lugarCompra="online";
+
+    var coche17=new Object();
+
+    coche17.marca="ford";
+    coche17.color="rojo";
+    coche17.placa="k 256";
+    coche17.motor="200 caballos";
+    coche17.precio="50 000";
+    coche17.pais="italia";
+    coche17.lugarCompra="online";
+
+    var coche18=new Object();
+
+    coche18.marca="ford";
+    coche18.color="rojo";
+    coche18.placa="k 256";
+    coche18.motor="200 caballos";
+    coche18.precio="50 000";
+    coche18.pais="italia";
+    coche18.lugarCompra="online";
+
+    var coche19=new Object();
+
+    coche19.marca="ford";
+    coche19.color="rojo";
+    coche19.placa="k 256";
+    coche19.motor="200 caballos";
+    coche19.precio="50 000";
+    coche19.pais="italia";
+    coche19.lugarCompra="online";
+
+    var coche20=new Object();
+
+    coche20.marca="ford";
+    coche20.color="rojo";
+    coche20.placa="k 256";
+    coche20.motor="200 caballos";
+    coche20.precio="50 000";
+    coche20.pais="italia";
+    coche20.lugarCompra="online";
+
+
+
+}
+
+
+function suma(){
+
+
+    var numero=document.getElementById("numero").value;
+    var mensaje=numero;
+
+    if (numero%2==0){
+        document.write("el numero es par")
+
+
+
+    }  else {
+        document.write("el numero es  inpar")
+
+
+    }
+}
